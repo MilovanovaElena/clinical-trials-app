0d50519931cfab58cad8aa9b535d1eaa534aa84a
Lena <angst606@mail.ru>
2025-11-28 15:11:34 +0300

    Remove old files

---
+++ clinical-trials-app/trials.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.trials (trial_id,trial_name,start_date,end_date,med) VALUES
+	 (1,'Trial 1','2024-01-11','2024-06-11',NULL),
+	 (2,'Trial 2','2024-01-21','2024-06-21',NULL),
+	 (3,'Trial 3','2024-01-31','2024-07-01',NULL),
+	 (4,'Trial 4','2024-02-10','2024-07-11',NULL),
+	 (5,'Trial 5','2024-02-20','2024-07-21',NULL),
+	 (6,'Trial 6','2024-03-01','2024-07-31',NULL),
+	 (7,'Trial 7','2024-03-11','2024-08-10',NULL);
